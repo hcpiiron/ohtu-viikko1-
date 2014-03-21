ca9847d61c76bdb857f62eb1d60a94b7d19c1fb3
Henri Piironen <piironen.henri@gmail.com>
2014-03-21 17:59:38 +0200

    sad

---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
@@ -0,0 +1,22 @@
+package ohtu.verkkokauppa;
+
+public class Pankki implements Bankkos  {
+
+
+    
+    private Kirjaus kirjanpito;
+
+    public Pankki(Kirjaus kirjanpito) {
+        this.kirjanpito = kirjanpito;
+    }
+
+    
+    @Override
+    public boolean tilisiirto(String nimi, int viitenumero, String tililta, String tilille, int summa) {
+        kirjanpito.lisaaTapahtuma("tilisiirto: tililtä " + tilille + " tilille " + tilille
+                + " viite " + viitenumero + " summa " + summa + "e");
+
+        // täällä olisi koodi joka ottaa yhteyden pankin verkkorajapintaan
+        return true;
+    }
+}
